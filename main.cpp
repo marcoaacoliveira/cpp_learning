e5bb681e1d0a7024d6f5b1656bbbb494ef305d25
marcoaacoliveira <marcoaacoliveira@gmail.com>
2015-05-26 15:07:28 -0300

    Primeiro programa C++ (Hello Wold)

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+void greetings(){
+    string name;
+    cin >> name;
+    cout << "Hello, "+name+"!\n" << endl;
+}
+
+int main() {
+    //greetings();
+    return 0;
+}
